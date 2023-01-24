be296e73c7bca98d514871435c55557ddf1cd968
Michael Assaf <michael.antoine.assaf@gmail.com>
2023-01-24 16:13:11 -0500

    master password

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,29 @@
 <script>
   import Accounts from "$lib/Accounts.svelte";
+
+  let passwordEntered = false;
+  let enteredPassword = "";
+
+  function verifyPassword() {
+    // check enteredPassword against stored master password
+    if (enteredPassword === "masterpassword") {
+      passwordEntered = true;
+    } else {
+      alert("Incorrect password. Please try again.");
+    }
+  }
 </script>
 
 <h1>Welcome to Frostbyte!</h1>
 
 <div>
-  <Accounts />
+  {#if !passwordEntered}
+    <div>
+      <label for="password">Enter Master Password:</label>
+      <input type="password" id="password" bind:value={enteredPassword} />
+      <button on:click={verifyPassword}>Submit</button>
+    </div>
+  {:else}
+    <Accounts />
+  {/if}
 </div>
